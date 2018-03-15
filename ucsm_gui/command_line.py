0d499404b324e5a4c384c40b1b0972e2e7083d7e
Bradley Jones <jones.bradley@me.com>
2018-03-15 12:53:59 +0000

    Convert name to hostname for login

--- ucsm_gui/command_line.py
+++ ucsm_gui/command_line.py
@@ -9,8 +9,8 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 def _get_host_from_conf(conf, host):
     if host in conf:
-        return conf[host]
-    return next((conf[item] for item in conf
+        return conf[host]["hostname"], conf[host]
+    return next(((conf[item]["hostname"], conf[item]) for item in conf
                 if conf[item]["hostname"] == host), None)
 
 
@@ -42,10 +42,10 @@ def main(config, host, username, password):
     else:  # Try to load from the default config path
         conf = cfg.load()
 
+    host, host_details = _get_host_from_conf(conf, host)
+
     if username and password:
         ucsm_gui.launch(host, username, password)
-
-    host_details = _get_host_from_conf(conf, host)
 
     if username:
         password = _get_password(host_details)
